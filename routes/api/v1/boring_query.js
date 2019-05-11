b2844ab0a085057cd3337f7dde6cee2bb493c074
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-05-11 15:06:02 -0600

    Adds framework for boring query recipe endpoint.

---
+++ routes/api/v1/boring_query.js
@@ -0,0 +1,63 @@
+var express = require("express");
+var router = express.Router();
+var Recipe = require('../../../models').Recipe;
+var BoringQuery = require('../../../models').BoringQuery;
+var BoringQueryRecipe = require('../../../models').BoringQueryRecipe;
+const fetch = require('node-fetch');
+pry = require('pryjs');
+
+router.get("/", async (req, res, next) => {
+  res.setHeader("Content-Type", "application/json");
+  const query = req.query.query
+  if (!query) {
+    res.status(404).send()
+  } else {
+    try {
+      // look in the BoringQuery table
+      const boringQueryInstance = await BoringQuery.findOne({
+        where: {
+          query: query
+        }
+      })
+      if (!boringQueryInstance) {
+        // fetch from Recipe from Edamam using query from the param
+      } else {
+        // find the matching recipes
+        try {
+          // look in the joins table
+          const boringQueryRecipeInstances = await BoringQueryRecipe.findAll({
+            where: {
+              BoringQueryId: boringQueryInstance.id
+            }
+          })
+          try {
+            // use the array of recipe id's to look at the Recipe table
+
+            // HOW TO YOU DO THIS FOR A PROMISE>ALL ... for the array of recipes
+            const recipes = await Recipe.findAll({
+              where: {
+                id: boringQueryRecipeInstances.id
+                // need to iterate through the above line or something
+              }
+            })
+
+          } catch (error) {
+            res.status(500).send({
+              error
+            })
+          }
+        } catch (error) {
+          res.status(500).send({
+            error
+          })
+        }
+      }
+    } catch (error) {
+      res.status(500).send({
+        error
+      })
+    }
+  }
+})
+
+module.exports = router;
