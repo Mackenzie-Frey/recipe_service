c0ccb8a4af3844b1adea87b85cbf80260a7c9fe6
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-05-11 13:37:52 -0600

    Adds spec helper for test setup and tear down.

---
+++ specs/spec_helper.js
@@ -0,0 +1,18 @@
+var shell = require('shelljs');
+
+const testSetup = () => {
+  shell.exec('npx sequelize db:create')
+  shell.exec('npx sequelize db:migrate')
+  shell.exec('npx sequelize db:seed:all')
+}
+
+const tearDown = () => {
+  shell.exec('npx sequelize db:seed:undo:all')
+  shell.exec('npx sequelize db:migrate:undo:all')
+  shell.exec('npx sequelize db:drop')
+}
+
+module.exports = {
+  testSetup,
+  tearDown
+}
