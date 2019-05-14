eaba0fbd984a5c61e6f4608af20123924ac0ee27
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-05-14 14:32:29 -0600

    Tests for additions to database upon successful request.

--- specs/endpoints/boring_query.spec.js
+++ specs/endpoints/boring_query.spec.js
@@ -1,6 +1,9 @@
 var specHelper = require('../spec_helper');
 var request = require("supertest");
 var app = require('../../app');
+var Recipe = require('../../models').Recipe;
+var BoringQuery = require('../../models').BoringQuery;
+var BoringQueryRecipe = require('../../models').BoringQueryRecipe;
 
 describe('BoringQuery Recipe index API', () => {
   describe('Test GET /api/v1/recipes?query=chicken path', () => {
@@ -10,6 +13,15 @@ describe('BoringQuery Recipe index API', () => {
     });
 
     test('it should return a 200 status', () => {
+      BoringQuery.findAll().then(response => {
+        expect(response.length).toBe(0)
+      })
+      BoringQueryRecipe.findAll().then(response => {
+        expect(response.length).toBe(0)
+      })
+      Recipe.findAll().then(response => {
+        expect(response.length).toBe(0)
+      })
       return request(app).get("/api/v1/recipes?query=chicken").then(response => {
         expect(response.status).toBe(200)
         expect(response.body).toBeInstanceOf(Array)
@@ -21,10 +33,19 @@ describe('BoringQuery Recipe index API', () => {
         expect(Object.keys(response.body[0])).toContain('image')
         expect(Object.keys(response.body[0])).toContain('totalTime')
         expect(Object.keys(response.body[0])).toContain('name')
+        BoringQuery.findAll().then(response => {
+          expect(response.length).toBe(1)
+        })
+        BoringQueryRecipe.findAll().then(response => {
+          expect(response.length).toBe(30)
+        })
+        return Recipe.findAll().then(response => {
+          expect(response.length).not.toBeLessThan(15)
+        })
       });
     });
 
-    test('it should return a 404 status and error message if query does not exist', () => {
+    test.skip('it should return a 404 status and error message if query does not exist', () => {
       return request(app).get("/api/v1/recipes").then(response => {
         expect(response.status).toBe(404)
         expect(response.body).toBe({
@@ -33,11 +54,10 @@ describe('BoringQuery Recipe index API', () => {
       })
     });
 
-    //Test for query, recipes and queryRecipe additions to database (1, 30, 30 respectively)
     //Test repeat query. Same response as original.
     //Test database entries after and that number of entries in database hasn't changed.
 
-    test('it should return a 404 status when unsuccessful', () => {
+    test.skip('it should return a 404 status when unsuccessful', () => {
       return request(app).get("/bad_path").then(response => {
         expect(response.statusCode).toBe(404)
       });
