e97c8214ac19db381ccc7b71ec1479c3b57904a3
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-05-13 19:49:31 -0600

    Removes comments per previous team discussion, as tests are working.

--- specs/spec_helper.js
+++ specs/spec_helper.js
@@ -3,13 +3,10 @@ var shell = require('shelljs');
 const testSetup = () => {
   shell.exec('npx sequelize db:create')
   shell.exec('npx sequelize db:migrate')
-  // shell.exec('npx sequelize db:seed:all')
 }
 
 const tearDown = () => {
-  // shell.exec('npx sequelize db:seed:undo:all')
   shell.exec('npx sequelize db:migrate:undo:all')
-  // shell.exec('npx sequelize db:drop')
 }
 
 module.exports = {
