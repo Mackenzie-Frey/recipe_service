e352b910960d29555a58871b52ab9de7546c57e9
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-05-14 20:35:10 -0600

    Utilizes beautify. Alters getRecipe method to utilize ES6 and async awaitt.

--- routes/api/v1/query.js
+++ routes/api/v1/query.js
@@ -12,7 +12,9 @@ const fetch = require('node-fetch');
 router.get("/", async (req, res, next) => {
   res.setHeader("Content-Type", "application/json");
   if (!req.query.query) {
-    res.status(404).send({error: "Missing recipe search query."})
+    res.status(404).send({
+      error: "Missing recipe search query."
+    })
   } else {
     const searchQuery = req.query.query.toLowerCase();
     // create url for Edamam using the query from the param
@@ -21,19 +23,23 @@ router.get("/", async (req, res, next) => {
     var url = `https://api.edamam.com/search?q=${searchQuery}&app_id=${appId}&app_key=${appKey}&to=30`
 
     return findOrFetchRecipes(searchQuery, BoringQuery, url, BoringQueryRecipe)
-    .then(recipes => {
-      res.status(200).send(JSON.stringify(recipes))
-    })
-    .catch(error => {
-      res.status(404).send({error: error})
-    })
+      .then(recipes => {
+        res.status(200).send(JSON.stringify(recipes))
+      })
+      .catch(error => {
+        res.status(404).send({
+          error: error
+        })
+      })
   }
 });
 
 router.get("/heart-attack", async (req, res, next) => {
   res.setHeader("Content-Type", "application/json");
   if (!req.query.query) {
-    res.status(404).send({error: "Missing recipe search query."})
+    res.status(404).send({
+      error: "Missing recipe search query."
+    })
   } else {
     const searchQuery = req.query.query.toLowerCase();
     // create url for Edamam using the query from the param
@@ -43,19 +49,23 @@ router.get("/heart-attack", async (req, res, next) => {
     var url = `https://api.edamam.com/search?q=${searchQuery}&app_id=${appId}&app_key=${appKey}&calories=2000-999999&to=30` + recipeFilter
 
     return findOrFetchRecipes(searchQuery, HeartAttackQuery, url, HeartAttackQueryRecipe)
-    .then(recipes => {
-      res.status(200).send(JSON.stringify(recipes))
-    })
-    .catch(error => {
-      res.status(404).send({error: error})
-    })
+      .then(recipes => {
+        res.status(200).send(JSON.stringify(recipes))
+      })
+      .catch(error => {
+        res.status(404).send({
+          error: error
+        })
+      })
   }
 });
 
 router.get("/bang-for-your-buck", async (req, res, next) => {
   res.setHeader("Content-Type", "application/json");
   if (!req.query.query) {
-    res.status(404).send({error: "Missing recipe search query."})
+    res.status(404).send({
+      error: "Missing recipe search query."
+    })
   } else {
     const searchQuery = req.query.query.toLowerCase();
     // create url for Edamam using the query from the param
@@ -65,97 +75,101 @@ router.get("/bang-for-your-buck", async (req, res, next) => {
     var url = `https://api.edamam.com/search?q=${searchQuery}&app_id=${appId}&app_key=${appKey}&to=30` + recipeFilter
 
     return findOrFetchRecipes(searchQuery, BBQuery, url, BBQueryRecipe)
-    .then(recipes => {
-      recipes.sort((a, b) => a.totalTime - b.totalTime);
-      res.status(200).send(JSON.stringify(recipes))
-    })
-    .catch(error => {
-      res.status(404).send({error: error})
-    })
+      .then(recipes => {
+        recipes.sort((a, b) => a.totalTime - b.totalTime);
+        res.status(200).send(JSON.stringify(recipes))
+      })
+      .catch(error => {
+        res.status(404).send({
+          error: error
+        })
+      })
   }
 });
 
-function findOrFetchRecipes(searchQuery, queryModel, url, queryRecipeModel){
+function findOrFetchRecipes(searchQuery, queryModel, url, queryRecipeModel) {
   return new Promise((resolve, reject) => {
     // look in the BoringQuery table for an existing query matching the request
     queryModel.findOne({
-      where: {
-        query: searchQuery
-      }
-    })
-    .then(query => {
-      //if it is a new query
-      if (!query) {
-        //fetch new recipes from edamam api
-        getRecipes(url)
-        .then(recipeResponse => {
-          //create BoringQuery, Recipes, and BoringQueryRecipes in database
-          return createQuery(recipeResponse, searchQuery, queryModel, queryRecipeModel);
-        })
-        .then(recipes => {
-          //Send newly retrieved recipes. Sort by highest calorie total and only top 10 results.
-          recipes.sort((a, b) => b.calories - a.calories);
-          recipes = recipes.slice(0, 10);
-          //res.status(200).send(JSON.stringify(recipes))
-          resolve(recipes)
-        })
-        .catch(error => {
-          reject(error)
-        })
-      //If an existing query is found
-      } else {
-        // find the recipes that are already associated with the existing query
-        Recipe.findAll({
-          include: [{
-            model: queryModel,
-            attributes: [],
-            where: {
-              id: query.id
-            }
-          }],
-          order: [['calories', 'DESC']],
-          limit: 10
-        })
-        .then(recipes => {
-          //Send retrieved recipes.
-          resolve(recipes)
-        })
-        .catch(error => {
-          reject(error)
-        })
-      }
-    })
-    .catch(error => {
-      error = "Bad search query."
-      reject(error)
-    })
+        where: {
+          query: searchQuery
+        }
+      })
+      .then(query => {
+        //if it is a new query
+        if (!query) {
+          //fetch new recipes from edamam api
+          getRecipes(url)
+            .then(recipeResponse => {
+              //create BoringQuery, Recipes, and BoringQueryRecipes in database
+              return createQuery(recipeResponse, searchQuery, queryModel, queryRecipeModel);
+            })
+            .then(recipes => {
+              //Send newly retrieved recipes. Sort by highest calorie total and only top 10 results.
+              recipes.sort((a, b) => b.calories - a.calories);
+              recipes = recipes.slice(0, 10);
+              //res.status(200).send(JSON.stringify(recipes))
+              resolve(recipes)
+            })
+            .catch(error => {
+              reject(error)
+            })
+          //If an existing query is found
+        } else {
+          // find the recipes that are already associated with the existing query
+          Recipe.findAll({
+              include: [{
+                model: queryModel,
+                attributes: [],
+                where: {
+                  id: query.id
+                }
+              }],
+              order: [
+                ['calories', 'DESC']
+              ],
+              limit: 10
+            })
+            .then(recipes => {
+              //Send retrieved recipes.
+              resolve(recipes)
+            })
+            .catch(error => {
+              reject(error)
+            })
+        }
+      })
+      .catch(error => {
+        error = "Bad search query."
+        reject(error)
+      })
   })
 };
 
-function getRecipes(url){
-  return new Promise((resolve, reject) => {
-    fetch(url)
-    .then(response => {
-      if (response.status === 200){
-        resolve(response.json())
-      }else{
-        reject(result.error_message)
-      }
-    })
-  })
-};
+const getRecipes = async (url) => {
+  try {
+    const fetchedThing = await fetch(url)
+    if (fetchedThing.status === 200) {
+      return await fetchedThing.json()
+    } else {
+      return fetchedThing.error_message
+    }
+  } catch (error) {
+    throw Error(error.error_message)
+  }
+}
 
-function createQuery(recipeResponse, searchQuery, queryModel, queryRecipeModel){
+function createQuery(recipeResponse, searchQuery, queryModel, queryRecipeModel) {
   return new Promise((resolve, reject) => {
     queryModel.create({
-      query: searchQuery
-    })
-    .then(query => {
-      resolve(parseRecipes(recipeResponse, query, queryRecipeModel));
-    })
-    .catch(error => {
-      reject(error)
-    })
+        query: searchQuery
+      })
+      .then(query => {
+        resolve(parseRecipes(recipeResponse, query, queryRecipeModel));
+      })
+      .catch(error => {
+        reject(error)
+      })
   })
 };
 
@@ -165,30 +179,31 @@ function parseRecipes(recipeResponse, query, queryRecipeModel) {
   }))
 };
 
-function createRecipe(recipe, query, queryRecipeModel){
+function createRecipe(recipe, query, queryRecipeModel) {
   return new Promise((resolve, reject) => {
     Recipe.findOrCreate({
-      where: {name: recipe.recipe.label},
-      defaults: {
-        name: recipe.recipe.label,
-        url: recipe.recipe.url,
-        yield: recipe.recipe.yield,
-        calories: Math.round((recipe.recipe.calories / recipe.recipe.yield)),
-        image: recipe.recipe.image,
-        totalTime: recipe.recipe.totalTime
-      }
-    })
-    .then(recipe => {
-      query.addRecipe(recipe[0])
-      .then(() => {
-        resolve(recipe[0])
+        where: {
+          name: recipe.recipe.label
+        },
+        defaults: {
+          name: recipe.recipe.label,
+          url: recipe.recipe.url,
+          yield: recipe.recipe.yield,
+          calories: Math.round((recipe.recipe.calories / recipe.recipe.yield)),
+          image: recipe.recipe.image,
+          totalTime: recipe.recipe.totalTime
+        }
       })
-    })
-    .catch((error) => {
-      reject(error)
-    })
+      .then(recipe => {
+        query.addRecipe(recipe[0])
+          .then(() => {
+            resolve(recipe[0])
+          })
+      })
+      .catch((error) => {
+        reject(error)
+      })
   })
 };
 
 module.exports = router;
-module.exports.getRecipes = getRecipes;
