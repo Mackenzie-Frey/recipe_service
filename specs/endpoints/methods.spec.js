754a53d275ac885891da47146657f17979458b64
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-05-14 20:29:26 -0600

    Adds test for getRecipe method. Currently skipped due to mocking out the fetch call not operating as epected.

--- specs/endpoints/methods.spec.js
+++ specs/endpoints/methods.spec.js
@@ -1,21 +1,19 @@
 var specHelper = require('../spec_helper');
 var request = require("supertest");
 var app = require('../../app');
-var queryFile = require('../../routes/api/v1/query.js');
-const fetch = require('node-fetch');
-jest.mock('node-fetch', () => jest.fn());
-// import * as methods from '../../routes/api/v1/query'
+var queryFile = require('../../routes/api/v1/query.js')
 
 describe('Methods for all query endpoints', () => {
-  test('getRecipes', () => {
+  // object being returned is {}, expected behavior is {test: 'thing'}
+  test.skip('getRecipes', () => {
 
-    fetch.mockImplementation(() =>
+  global.fetch = jest.fn().mockImplementation(() =>
     Promise.resolve({
       status: 200,
       json: () => Promise.resolve({test: 'thing'})
     })
     )
-
-    expect(queryFile.getRecipes('www.test.com')).toBe(0)
+console.log(global.fetch)
+    expect(queryFile.getRecipes('www.test.com')).toBe({test: 'thing'})
   })
 })
